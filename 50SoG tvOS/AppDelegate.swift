737cba73163a92d6c4ea78bb6ea41e4b7daf0f0d
Alexander von Below <alexander.von_below@telekom.de>
2020-06-18 08:39:19 +0200

    [M] TV App updated

---
+++ 50SoG tvOS/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  50SoG tvOS
+//
+//  Created by Alexander v. Below on 18.06.20.
+//  Copyright © 2020 Alexander von Below. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = SG50View()
+
+        // Use a UIHostingController as window root view controller.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UIHostingController(rootView: contentView)
+        self.window = window
+        window.makeKeyAndVisible()
+        return true
+    }
+
+}
+
